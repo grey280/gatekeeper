77ecd68fc0fb86124b8d32bf25ad95c039f84141
grey280 <greypatterson@gmail.com>
2021-01-09 11:00:30 -0800

    Match Vapor compatibility requirements

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "Gatekeeper",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "Gatekeeper",
@@ -15,7 +18,7 @@ let package = Package(
         .target(
             name: "Gatekeeper",
             dependencies: [
-                "Vapor"
+                .product(name: "Vapor", package: "vapor")
             ]),
         .testTarget(
             name: "GatekeeperTests",
